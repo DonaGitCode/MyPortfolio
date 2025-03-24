2b7dc36a2a4723e1024e5d898a5e8ce1ffd36376
Donato Nicolas Ayala Osorio <100354710+DonaGitCode@users.noreply.github.com>
2025-03-24 14:41:58 +0000

    NEW

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/MyPortfolio/", // Nombre del repositorio en GitHub
+  base: "./", // Usa "./" para evitar problemas con rutas absolutas
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
-
